7fdc3db73e248d40634bb7e3ccc035276a345ef8
Delphine <delphinegrenier98@gmail.com>
2024-10-21 01:10:04 -0400

    ajout content

--- script.js
+++ script.js
@@ -1,49 +1,90 @@
 gsap.registerPlugin(ScrollTrigger);
 let items = document.querySelectorAll('.competences__item');
-let text = document.querySelector('.competences__text'); 
-let testItems = document.querySelectorAll('.test .test__item'); // Select items in the test class
+let text = document.querySelector('.competences'); 
+let button = document.querySelector('.btn');
 
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 50,
+    initialSlide: 1,
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+      delay: 5500,
+      disableOnInteraction: false,
+    },
+    freeMode: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
 
-
-items.forEach(item => {
-    item.addEventListener('mousemove', (e) => {
-        const rect = item.getBoundingClientRect();
-        const x = e.clientX - rect.left - rect.width / 2;
-        const y = e.clientY - rect.top - rect.height / 2;
-
-        gsap.to(item, {
-            x: x * 0.2,
-            y: y * 0.3, 
-            duration: 0.3, 
-            ease: "power3.out"
-        });
-    });
-
-    item.addEventListener('mouseleave', () => {
-        gsap.to(item, {
-            x: 0,
-            y: 0,
-            duration: 0.3,
-            ease: "power3.out"
-        });
-    });
-});
+  var swiper = new Swiper(".myProjet", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    loop: true,
+    autoplay: {
+      delay: 5500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
 
 
 
 
 
 gsap.fromTo(text, {
-y: 900,
+y: 500,
 },
 {
-    y: -300, // Adjust the speed for upward movement of text
+    y: 200, 
     ease: "none",
     scrollTrigger: {
-        trigger: '.competences',
+        trigger: '.hidden-competences',
         start: "top bottom",
         end: "bottom top",
         scrub: 1         
     }
 });
 
+
+
+
+
+items.forEach((item, index) => {
+  gsap.to(item, {
+    y: -(index + 1) * 10, 
+    x: (index % 2 === 0 ? -1 : 1) * 60,
+    ease: "none",
+    scrollTrigger: {
+      trigger: '.competences', 
+    toggleActions: 'restart complete reverse reset',
+      start: "top bottom", 
+      end: "bottom top",   
+      scrub: 1         
+}});
+});
+
+button.addEventListener('mousemove', (e) => {
+  const rect = button.getBoundingClientRect();
+  const x = e.clientX - rect.left - rect.width / 2;
+  const y = e.clientY - rect.top - rect.height / 2;
+
+  button.style.transform = `translate(${x / 5}px, ${y / 5}px)`;
+});
+
+button.addEventListener('mouseleave', () => {
+  button.style.transform = 'translate(0, 0)';
+});
+
+    
+    
